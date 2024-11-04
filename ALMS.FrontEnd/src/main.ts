bef6a5f68781d3c00d7a234a569c861ecc693a64
Shaq03 <93329271+Shaq03@users.noreply.github.com>
2024-11-04 16:11:31 +0000

    login page added (#14)

--- ALMS.FrontEnd/src/main.ts
+++ ALMS.FrontEnd/src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import LandingView from '@/views/LandingView.vue'
 import RegistrationView from './views/RegistrationView.vue'
+import LoginView from './views/LoginView.vue'
 
 
 
@@ -13,5 +14,6 @@ const app = createApp(App)
 app.use(router)
 app.component('LandingView', LandingView)
 app.component('RegistrationView', RegistrationView)
+app.component('LoginView', LoginView)
 
 app.mount('#app')
--- ALMS.FrontEnd/src/router/index.ts
+++ ALMS.FrontEnd/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingView from '@/views/LandingView.vue'
 import RegistrationView from '@/views/RegistrationView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/registration',
       name: 'registration',
       component: RegistrationView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
     },
   ]
 })
