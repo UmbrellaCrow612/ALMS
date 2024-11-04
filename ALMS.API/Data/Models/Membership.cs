c70da3198b0302135e5839f2e2049d4a122592a1
Arian <ariandehghani89@gmail.com>
2024-11-04 16:33:22 +0000

    Attributes Added to the Models

--- ALMS.API/Data/Models/Membership.cs
+++ ALMS.API/Data/Models/Membership.cs
@@ -2,5 +2,21 @@
 {
     public class Membership
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public required DateTime StartDate { get; set; } = DateTime.UtcNow;
+        public DateTime? EndDate { get; set; }
+
+        public required decimal MonthlyFee { get; set; }
+
+
+    }
+
+    public enum MembershipLevel
+    {
+        None = 0,
+        Basic = 1,
+        Silver = 2,
+        Gold = 3
     }
 }
