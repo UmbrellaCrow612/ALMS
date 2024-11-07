a3f2601afcae896705ffd97599d406400cd96403
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-07 09:21:46 +0000

    Create User in DB

--- ALMS.API/Controllers/AuthenticationController.cs
+++ ALMS.API/Controllers/AuthenticationController.cs
@@ -1,17 +1,38 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ALMS.API.Data.Models;
+using ALMS.API.DTOs.Users;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ALMS.API.Controllers
 {
     [ApiController]
     [Route("auth")]
-    public class AuthenticationController : ControllerBase
+    public class AuthenticationController(IMapper mapper, UserManager<ApplicationUser> userManager) : ControllerBase
     {
+        private readonly IMapper _mapper = mapper;
+        private readonly UserManager<ApplicationUser> _userManager = userManager;
+
+
         [AllowAnonymous]
         [HttpPost("register")]
-        public async Task<ActionResult> Register()
+        public async Task<ActionResult<IdentityResult>> Register([FromBody] CreateUserDto createUserDto)
         {
-            return Ok();
+            var userToCreate = _mapper.Map<ApplicationUser>(createUserDto);
+
+            var result = await _userManager.CreateAsync(userToCreate, createUserDto.Password);
+
+            if (result.Succeeded)
+            {
+                // User creation successful
+                return Ok(result);
+            }
+            else
+            {
+                // Handle errors
+                return BadRequest(result);
+            }
         }
     }
 }
--- ALMS.API/DTOs/Users/CreateUserDto.cs
+++ ALMS.API/DTOs/Users/CreateUserDto.cs
@@ -1,6 +1,28 @@
-﻿namespace ALMS.API.DTOs.Users
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ALMS.API.DTOs.Users
 {
     public class CreateUserDto
     {
+        [Required]
+        public required string Username { get; set; }
+
+        [Required]
+        public required string Email { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+
+        [Required]
+        public required string FirstName { get; set; }
+
+        [Required]
+        public required string LastName { get; set; }
+
+        [Required]
+        public required string Address { get; set; }
+
+        [Required]
+        public required int MaxBorrowLimit { get; set; }
     }
 }
