c22a0c2893e232ae76ffcf9b650ab9d6614074a3
Arian <93319255+arianhit@users.noreply.github.com>
2024-11-14 18:26:10 +0000

    Add Subscription and stripe entity framework (#26)
    * Add Subscription and stripe entity framework
    
    * add await _dbContext.StripeProducts.ToListAsync()

---
+++ ALMS.API/DTOs/Subscription/UpdateStripeProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ALMS.API.DTOs.Subscription
+{
+    public class UpdateStripeProductDto
+    {
+        [Required]
+        public required string Product { get; set; }
+        [Required]
+        public required long Quanity { get; set; }
+        [Required]
+        public required string Rate { get; set; }
+    }
+}
