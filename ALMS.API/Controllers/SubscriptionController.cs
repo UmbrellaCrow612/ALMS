299d92cc183157bf36a792b945e8220c54144448
Arian <ariandehghani89@gmail.com>
2024-11-14 17:39:26 +0000

    Add Subscription and stripe entity framework

--- ALMS.API/Controllers/SubscriptionController.cs
+++ ALMS.API/Controllers/SubscriptionController.cs
@@ -1,12 +1,17 @@
 ﻿using ALMS.API.Core.Constants;
 using ALMS.API.Data;
 using ALMS.API.Data.Models;
+using ALMS.API.DTOs.Media;
+using ALMS.API.DTOs.Subscription;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 using Stripe;
 using Stripe.Checkout;
+using System.Globalization;
 using System.Security.Claims;
 using Subscription = ALMS.API.Data.Models.Subscription;
 
@@ -14,13 +19,14 @@ namespace ALMS.API.Controllers
 {
     [ApiController]
     [Route("subscription")]
-    public class SubscriptionController(ApplicationDbContext dbContext) : ControllerBase
+    public class SubscriptionController(ApplicationDbContext dbContext, IMapper mapper) : ControllerBase
     {
         private readonly ApplicationDbContext _dbContext = dbContext;
+        private readonly IMapper _mapper = mapper;
 
         [Authorize]
-        [HttpPost("subscribe")]
-        public async Task<ActionResult> Subscribe()
+        [HttpPost("subscribe{id}")]
+        public async Task<ActionResult> Subscribe(string id)
         {
 
             var lineItems = new List<SessionLineItemOptions>();
@@ -38,20 +44,24 @@ namespace ALMS.API.Controllers
             // Loop through products to add them to the session
             foreach (var product in _dbContext.StripeProducts)
             {
-                var sessionListItem = new SessionLineItemOptions
+                if (product.Id == id) 
                 {
-                    PriceData = new SessionLineItemPriceDataOptions
+                    decimal price = decimal.TryParse(product.Rate.Replace("£", ""), NumberStyles.Any, CultureInfo.InvariantCulture, out decimal result) ? result : 0m;
+                    var sessionListItem = new SessionLineItemOptions
                     {
-                        UnitAmount = (long)(4.99 * 100), // Stripe expects the amount in cents
-                        Currency = "GBP",
-                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        PriceData = new SessionLineItemPriceDataOptions
                         {
-                            Name = product.Product.ToString(),
-                        }
-                    },
-                    Quantity = product.Quanity
-                };
-                options.LineItems.Add(sessionListItem);
+                            UnitAmount = (long)(price * 100), // Stripe expects the amount in cents
+                            Currency = "GBP",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = product.Product.ToString(),
+                            }
+                        },
+                        Quantity = product.Quanity
+                    };
+                    options.LineItems.Add(sessionListItem);
+                }
             }
 
             var service = new Stripe.Checkout.SessionService();
@@ -70,7 +80,7 @@ namespace ALMS.API.Controllers
             return Ok(redirectUrl);
         }
 
-        [Authorize]
+        [Authorize(Roles = UserRoles.Accountant)]
         [HttpGet("subscribersDetails")]
         public async Task<ActionResult> GetSubscribersDetails()
         {
@@ -102,6 +112,75 @@ namespace ALMS.API.Controllers
             {
                 return Ok(subscriptions);
             }
+        }
+
+        [Authorize]
+        [HttpGet("mysubscription")]
+        public async Task<ActionResult> GetSub()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var subscriptionDetails = await _dbContext.Subscriptions.Where(x => x.UserId == userId).ToListAsync();
+
+            return Ok(subscriptionDetails);
+        }
+
+        [Authorize(Roles = UserRoles.Accountant)]
+        [HttpPatch("update{id}")]
+        public async Task<ActionResult> UpdateSub([FromBody] UpdateSubscriptionDto updateSubscriptionDto,string id)
+        {
+
+            var subscriptonToUpdate = await _dbContext.Subscriptions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (subscriptonToUpdate is null) return NotFound();
+
+            _mapper.Map(updateSubscriptionDto, subscriptonToUpdate);
+
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [Authorize(Roles = UserRoles.Accountant)]
+        [HttpPost("stripeSubscription/create")]
+        public async Task<ActionResult> CreateStripeProduct([FromBody] CreateStripeProductDto createStripeProductDto)
+        {
+
+            var stripeProduct = _mapper.Map<StripeProductEntity>(createStripeProductDto);
+
+            await _dbContext.StripeProducts.AddAsync(stripeProduct);
+            await _dbContext.SaveChangesAsync();
+
+            return Created(nameof(createStripeProductDto), new { stripeProduct.Id });
+        }
+
+        [Authorize(Roles = UserRoles.Accountant)]
+        [HttpPatch("stripeSubscription/update/{id}")]
+        public async Task<ActionResult> UpdateStripeProduct([FromBody] UpdateStripeProductDto updateStripeProductDto, string id)
+        {
+
+            var StripeProductToUpdate = await _dbContext.StripeProducts.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (StripeProductToUpdate is null) return NotFound();
+
+            _mapper.Map(updateStripeProductDto, StripeProductToUpdate);
+
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [Authorize(Roles = UserRoles.Accountant)]
+        [HttpDelete("stripeSubscription/delete/{id}")]
+        public async Task<ActionResult> DeleteStripeSubscriptionItem(string id)
+        {
+            var stripeSubscriptionToDelete = await _dbContext.StripeProducts.FirstOrDefaultAsync(x => x.Id == id);
+            if (stripeSubscriptionToDelete is null) return NotFound();
+
+            _dbContext.StripeProducts.Remove(stripeSubscriptionToDelete);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
