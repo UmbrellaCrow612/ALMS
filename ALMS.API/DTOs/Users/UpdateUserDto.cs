1551cb03357f5122074d61cbab0d13b9b4cca20b
Arian <ariandehghani89@gmail.com>
2024-11-18 14:52:48 +0000

    AD-AddingProfileUpdate

--- ALMS.API/DTOs/Users/UpdateUserDto.cs
+++ ALMS.API/DTOs/Users/UpdateUserDto.cs
@@ -1,6 +1,25 @@
-﻿namespace ALMS.API.DTOs.Users
+﻿using ALMS.API.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ALMS.API.DTOs.Users
 {
     public class UpdateUserDto
     {
+
+        [Required]
+        public required string FirstName { get; set; }
+
+        [Required]
+        public required string LastName { get; set; }
+
+        [Required]
+        public required string Address { get; set; }
+        /// <summary>
+        /// Higher privilege will activate a user account / in the system treat this model as account 
+        /// </summary>
+
+        [Required]
+        [EnumDataType(typeof(MembershipStatus))]
+        public required MembershipStatus MembershipStatus { get; set; }
     }
 }
