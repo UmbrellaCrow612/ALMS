d64495760de272882f115dcdc380ea23864901f0
Yousaf Wazir <umbrellaman612@gmail.com>
2024-11-20 13:16:18 +0000

    Update UserDto.cs

--- ALMS.API/DTOs/Users/UserDto.cs
+++ ALMS.API/DTOs/Users/UserDto.cs
@@ -2,6 +2,7 @@
 {
     public class UserDto
     {
+        public required string Id { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string Address { get; set; }
