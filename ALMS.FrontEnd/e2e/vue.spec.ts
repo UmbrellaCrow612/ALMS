3c2d7dd578fb2bf1f173d79d65ab7a2f03de6bbf
Shaq03 <93329271+Shaq03@users.noreply.github.com>
2024-12-04 12:34:27 +0000

    added error mess to borrow media

--- ALMS.FrontEnd/e2e/vue.spec.ts
+++ ALMS.FrontEnd/e2e/vue.spec.ts
@@ -2,26 +2,27 @@ import { test, expect } from '@playwright/test';
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
-test('Use case one end to end test', async ({ page }) => {
+test('test for use case 1', async ({ page }) => {
   await page.goto('http://localhost:5173/');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.getByLabel('Email address').click();
-  await page.getByLabel('Email address').fill('librarymember@example.com');
+  await page.getByLabel('Email address').fill('branchlibrarian@example.com');
   await page.getByLabel('Password').click();
-  await page.getByLabel('Password').fill('MemberP@ssw0rd');
+  await page.getByLabel('Password').fill('SecureP@ssw0rd');
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.getByRole('link', { name: 'Search' }).click();
   await page.getByPlaceholder('Search by title').click();
-  await page.getByPlaceholder('Search by title').fill('med');
+  await page.getByPlaceholder('Search by title').press('CapsLock');
+  await page.getByPlaceholder('Search by title').fill('S');
+  await page.getByPlaceholder('Search by title').press('CapsLock');
+  await page.getByPlaceholder('Search by title').fill('Say');
+  await page.getByPlaceholder('Search by title').press('Enter');
   await page.getByRole('button', { name: 'Search' }).click();
+  await expect(page.getByRole('heading', { name: 'Sayings and anecdotes' })).toBeVisible();
   await page.getByPlaceholder('Search by title').click();
-  await page.getByPlaceholder('Search by title').fill('');
+  await page.getByPlaceholder('Search by title').fill('gdbgdbd');
   await page.getByRole('button', { name: 'Search' }).click();
-  await page.getByRole('button', { name: 'View Details' }).first().click();
-  await page.getByRole('link', { name: 'Search' }).click();
-})
+  await expect(page.getByText('No results found.')).toBeVisible();
+});
 
 
-test('Use case two end to end test', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-})
