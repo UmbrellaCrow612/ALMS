0307df22fb280c4677dddaacfd276ce62c0eec53
Yousaf Wazir <umbrellaman612@gmail.com>
2024-12-05 13:57:24 +0000

    Update yousafs-tests.spec.ts

--- ALMS.FrontEnd/e2e/yousafs-tests.spec.ts
+++ ALMS.FrontEnd/e2e/yousafs-tests.spec.ts
@@ -1,32 +1,52 @@
 import { test, expect } from '@playwright/test'
 
 test('Use case 3', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByLabel('Email address').click();
+  await page.getByLabel('Email address').fill('branchlibrarian@example.com');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('SecureP@ssw0rd');
+  await page.getByRole('button', { name: 'Sign in' }).click();
   await page.getByRole('link', { name: 'Inventory' }).click();
   await page.getByRole('button', { name: 'Add Media' }).click();
   await page.locator('div').filter({ hasText: /^Title$/ }).getByRole('textbox').click();
-  await page.locator('div').filter({ hasText: /^Title$/ }).getByRole('textbox').fill('oijwejd');
+  await page.locator('div').filter({ hasText: /^Title$/ }).getByRole('textbox').fill('test');
   await page.locator('textarea').click();
-  await page.locator('textarea').fill('oidwojd');
-  await page.locator('input[type="date"]').fill('1200-12-12');
+  await page.locator('textarea').fill('test');
+  await page.locator('input[type="date"]').fill('2000-12-12');
   await page.locator('div').filter({ hasText: /^ISBN$/ }).getByRole('textbox').click();
-  await page.locator('div').filter({ hasText: /^ISBN$/ }).getByRole('textbox').fill('123456789');
+  await page.locator('div').filter({ hasText: /^ISBN$/ }).getByRole('textbox').fill('1234567');
+  await page.locator('input[type="url"]').click();
   await page.locator('input[type="url"]').click();
   await page.locator('input[type="url"]').fill('https://picsum.photos/200/300');
   await page.locator('div').filter({ hasText: /^Author$/ }).getByRole('textbox').click();
-  await page.locator('div').filter({ hasText: /^Author$/ }).getByRole('textbox').fill('dewwd');
-  await page.locator('div').filter({ hasText: /^Genre$/ }).click();
+  await page.locator('div').filter({ hasText: /^Author$/ }).getByRole('textbox').fill('test');
+  await page.locator('div').filter({ hasText: /^Genre$/ }).getByRole('textbox').click({
+    button: 'right'
+  });
   await page.locator('div').filter({ hasText: /^Genre$/ }).getByRole('textbox').click();
-  await page.locator('div').filter({ hasText: /^Genre$/ }).getByRole('textbox').fill('wdewd');
-  await page.getByRole('combobox').selectOption('2');
+  await page.locator('div').filter({ hasText: /^Genre$/ }).getByRole('textbox').fill('test');
+  await page.getByRole('combobox').selectOption('1');
   await page.getByRole('button', { name: 'Create' }).click();
   await page.getByPlaceholder('Search inventory...').click();
-  await page.getByPlaceholder('Search inventory...').fill('oi');
-  await expect(page.getByRole('cell', { name: 'oijwejd' })).toBeVisible();
-  await page.getByRole('button').nth(2).click();
-  await page.locator('div').filter({ hasText: /^Title$/ }).getByRole('textbox').click();
-  await page.locator('div').filter({ hasText: /^Title$/ }).getByRole('textbox').fill('test man 12');
-  await page.getByRole('button', { name: 'Update' }).click();
+  await page.getByPlaceholder('Search inventory...').fill('test');
+  await expect(page.getByRole('cell', { name: 'test' }).first()).toBeVisible();
+  await page.getByPlaceholder('Search inventory...').click();
+  await page.getByPlaceholder('Search inventory...').fill('not made');
   await page.getByPlaceholder('Search inventory...').click();
   await page.getByPlaceholder('Search inventory...').fill('test');
-  await expect(page.getByRole('cell', { name: 'test man' })).toBeVisible();
+  await page.getByRole('button').nth(2).click();
+  await page.locator('div').filter({ hasText: /^Title$/ }).getByRole('textbox').click();
+  await page.locator('div').filter({ hasText: /^Title$/ }).getByRole('textbox').fill('test edited');
+  await page.getByRole('button', { name: 'Update' }).click();
+  await expect(page.getByRole('cell', { name: 'test edited' })).toBeVisible();
+  await page.getByRole('button').nth(3).click();
+  await page.getByRole('button', { name: 'Confirm' }).click();
 });
+
+// add login 
+
+// add register 
+
+// add forgot password
